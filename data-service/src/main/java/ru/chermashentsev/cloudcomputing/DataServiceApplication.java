6993a0824636c103909c70934d358210f348adfa
Ancherm <andrewcherm2014@gmail.com>
2025-05-06 18:21:29 +0400

    init project

---
+++ data-service/src/main/java/ru/chermashentsev/cloudcomputing/DataServiceApplication.java
@@ -0,0 +1,11 @@
+package ru.chermashentsev.cloudcomputing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DataServiceApplication.class, args);
+    }
+}
